ef605defb03badfecf073a18bb67a3a9c9598026
Alex <yahiaoui-martinez.alex@geneseng.com>
2023-01-16 19:25:53 +0100

    Fix fusionMultiPlot()

--- R/fusionMultiPlot.R
+++ R/fusionMultiPlot.R
@@ -109,7 +109,7 @@ fusionMultiPlot <- function(data,x, y, col = NULL, type = "msstepline", numberSu
       list(
         seriesname = n[z], 
         data = data.frame( 
-          value = as.character(as.numeric(factor(data[data[,col] == n[z],y])))
+          value = as.character(data[data[,col] == n[z],y])
         )
       )
     })
